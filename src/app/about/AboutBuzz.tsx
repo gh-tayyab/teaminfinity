f9ff631ad868c2876e4e2420060cabe32610fb81
gh-tayyab <muhammadtayyabmt0@gmail.com>
2025-10-01 18:50:47 +0500

    in progress

--- src/app/about/AboutBuzz.tsx
+++ src/app/about/AboutBuzz.tsx
@@ -16,7 +16,7 @@ export default function AboutBuzzz() {
       {/* Decorative Blur Layer */}
       <div
         aria-hidden="true"
-        className="absolute bottom-[-100px] right-[-150px] w-[350px] h-[350px] bg-[#00B7CD] rounded-full blur-3xl"
+        className="absolute bottom-[80px] right-[-300px] w-[350px] h-[150px] bg-[#00B7CD] rounded-full blur-3xl z-10"
       />
 
       <div className="relative z-10 w-full max-w-7xl mx-auto flex flex-col md:flex-row items-start md:items-center gap-8">
